96e58e13162487a99c9c88c4ec2ae1cf30f3bfb2
Vinicius Reis <vinireis010305@gmail.com>
2025-02-11 20:55:16 -0300

    Refactor DynamoDB table definitions for user, group, and invites

--- back-end/services/infra/main.tf
+++ back-end/services/infra/main.tf
@@ -13,26 +13,35 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_dynamodb_table" "vi_todo_dev_table" {
-  name         = "vi_todo_dev"
+resource "aws_dynamodb_table" "vi_todo_dev_users_table" {
+  name         = "vi-todo-dev-users-table"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_id"
-  range_key    = "group_id"
+  hash_key     = "id"
 
   attribute {
-    name = "user_id"
+    name = "id"
     type = "S"
   }
+}
+
+resource "aws_dynamodb_table" "vi_todo_dev_groups_table" {
+  name         = "vi-todo-dev-groups-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
-    name = "group_id"
+    name = "id"
     type = "S"
   }
+}
 
-  global_secondary_index {
-    name               = "group_id_index"
-    hash_key           = "group_id"
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["group_description", "tasks", "users", "group_title", "invetes"]
+resource "aws_dynamodb_table" "vi_todo_dev_invetes_table" {
+  name         = "vi-todo-dev-invetes-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
   }
 }
